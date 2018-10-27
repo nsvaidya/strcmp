9c2704ae071ff09273db9ec665614dad9a64679c
Sravan Paritala <sravanbrahma@yahoo.com>
2018-10-27 12:32:01 +0530

    Sravan, Prarthana Set Changes to Str compare Commit

--- find.py
+++ find.py
@@ -1,12 +1,12 @@
 # find.py - A small python utility for comparing strings of multiple files located in single folder.
 # Created by Neerad Vaidya on 9th Feb, 2018 15:58:15 IST
 # Distributed under GNU GPLv3 Licence
-import os, timeit,ctypes,sys,datetime
+import os, timeit,ctypes,sys,datetime,time
 ctypes.windll.shell32.ShellExecuteW(None,"runas",sys.executable,"",None,1)
 dat=datetime.datetime.now()
 print "Script started @ ",dat
 st=timeit.default_timer()
-simstri=list()
+simstri=set()
 fl=list()
 chkfl=list()
 for root, dirs, files in os.walk(os.getcwd()):
@@ -15,51 +15,36 @@ for root, dirs, files in os.walk(os.getcwd()):
 			fl.append(names)			
 lent=len(fl)
 print "File listing & indexing Complete"
+starttime=time.time()
 for a in range(0,lent):
-		for b in range(1,lent):
-			if a==b:
-				print "Same File, Can't Compare"
-				continue
+	for b in range(1,lent):
+		if a==b:
+			print "Same File, Can't Compare"
+			continue
+		else:
+			if (a,b) not in chkfl and (b,a) not in chkfl:
+				print "Analyzing File: " + fl[a] + " with " + fl[b]
+				filhan1=open(fl[a],"r")
+				filhan2=open(fl[b],"r")
+				#lst1=list()
+				#lst2=list()
+				hm1 = set()
+				for line in filhan1:
+					hm1.add(line)
+				for line in filhan2:
+					#lst2.append(line)
+					if line in hm1:
+						print "Match Found"
+						simstri.add(line)
+					
+				chkfl.append((a,b))
+				chkfl.append((b,a))
+				filhan1.close()
+				filhan2.close()
 			else:
-				if (a,b) not in chkfl and (b,a) not in chkfl:
-					print "Analyzing File: " + fl[a] + " with " + fl[b]
-					filhan1=open(fl[a],"r")
-					filhan2=open(fl[b],"r")
-					lst1=list()
-					lst2=list()
-					for line in filhan1:
-						lst1.append(line)
-					for line in filhan2:
-						lst2.append(line)
-					filhan1.close()
-					filhan2.close()
-					if len(lst1) >= len(lst2):
-						for items in lst1:
-							if items in lst2:
-								if items not in simstri:
-									print "Match Found"
-									simstri.append(items)
-								else:
-									print "Duplicate Match Found"
-									continue
-						chkfl.append((a,b))
-						chkfl.append((b,a))
-					elif len(lst1) <= len(lst2):
-						for items in lst2:
-							if items in lst1:
-								if items not in simstri:
-									print "Match Found"
-									simstri.append(items)
-								else:
-									print "Duplicate Match Found"
-									continue
-						chkfl.append((a,b))
-						chkfl.append((b,a))
-					else:
-						print "Unspecified Error"
-				else:
-					print "Files " + fl[a] + " with " + fl[b] + " are already checked"
+				print "Files " + fl[a] + " with " + fl[b] + " are already checked"
 fhand=open("result.txt","w")
+print(str((time.time()-starttime)*1000))
 for items in simstri:
 	fhand.write(items)
 fhand.close()
